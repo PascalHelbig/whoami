b377f5ab953922bea86127a910bd788c74dd4d5f
pascalhelbig <pascalhelbig@gmail.com>
2016-02-01 11:15:11 +0100

    add browser

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var acceptLanguage = require('accept-language');
+var userAgent = require('express-useragent');
 var app = express();
+
+app.use(userAgent.express());
 
 app.get('/', function (req, res) {
   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   var language = acceptLanguage.parse(req.headers["accept-language"]);
+  var browser = req.useragent.os;
   res.json({
     ipaddress: ip,
-    language: language[0].language
+    language: language[0].language,
+    browser: browser
   });
 });
 
